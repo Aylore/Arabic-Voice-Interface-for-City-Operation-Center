e9cc77188c522f10f25a429449d3985dac72708e
Osama Fayez <osamaoun997@gmail.com>
2023-05-22 12:58:15 +0300

    Azure speech to text model

---
+++ utils/azure_models/azure_speech_to_text.py
@@ -0,0 +1,43 @@
+import azure.cognitiveservices.speech as speechsdk
+import os
+
+# Model class
+class Azure_stt_model:
+    """A model that taker audio path and returns text"""
+    
+    def __init__(self):
+        # Set up the subscription info for the Speech Service:
+        self.__speech_key, self.__service_region = os.getenv('azure_stt_key'), "eastus"
+        
+        # Create an instance of a speech config with specified subscription key and service region.
+        self.speech_config = speechsdk.SpeechConfig(subscription=self.__speech_key, region=self.__service_region)
+        
+        # identifying the language in an audio source 
+        self.auto_detect_source_language_config = speechsdk.languageconfig.AutoDetectSourceLanguageConfig(languages=["en-US", "ar-EG"])
+        
+    def predict(self, path : str) -> str:
+        """path: Path to audio file"""
+        
+        # recognize speech from an audio file
+        audio_config = speechsdk.audio.AudioConfig(filename=path)
+
+        # Create a recognizer with the given settings.
+        speech_recognizer = speechsdk.SpeechRecognizer(
+        speech_config=self.speech_config, 
+        auto_detect_source_language_config=self.auto_detect_source_language_config, 
+        audio_config=audio_config)
+        
+        # getting results
+        result = speech_recognizer.recognize_once()
+        
+        # returning results
+        if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+            print("Text generated")
+            return result.text
+        elif result.reason == speechsdk.ResultReason.NoMatch:
+            print("No speech could be recognized: {}".format(result.no_match_details))
+        elif result.reason == speechsdk.ResultReason.Canceled:
+            cancellation_details = result.cancellation_details
+            print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+            if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                print("Error details: {}".format(cancellation_details.error_details))
