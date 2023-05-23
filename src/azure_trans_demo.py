7312b7ca6de124499db89f1355748461a665f955
Osama Fayez <osamaoun997@gmail.com>
2023-05-23 23:39:20 +0300

    Azure translator and demo

---
+++ src/azure_trans_demo.py
@@ -0,0 +1,12 @@
+from utils.azure_translator import Translator
+
+# Example usage:
+translator = Translator()
+input_text1 = "I love english"
+input_text2 = "انا احب العربية"
+
+translated_text1 = translator.translate_text(input_text1)
+translated_text2 = translator.translate_text(input_text2)
+
+print(f"Original text: {input_text1}, Translated Text:", translated_text1)
+print(f"Original text: {input_text2}, Translated Text:", translated_text2)
