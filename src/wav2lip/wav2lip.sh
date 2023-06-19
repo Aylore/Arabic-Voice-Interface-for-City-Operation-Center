3631368fab90eaaff32de8cb97059cfe5160dfe1
Aylore <ahmedelghitany.2274@gmail.com>
2023-06-19 19:40:21 +0300

    add wav2lip inference script

--- src/wav2lip/wav2lip.sh
+++
@@ -1,1 +0,0 @@
-cd Wav2Lip && python wav2lip_train.py --data_root "$preprocessed_path" --checkpoint_dir /content/drive/MyDrive/wav2lib/Wav2Lip\ videos/train/wav2lip_checkpoints/ --syncnet_checkpoint_path /content/drive/MyDrive/wav2lib/Wav2Lip\ videos/train/checkpoints/checkpoint_step000016000.pth
